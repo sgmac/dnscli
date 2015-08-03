7e3c578502db38d455efe7bfd2651bc3b3e558d6
Sergio Galvan <sergalma@gmail.com>
2015-08-03 22:59:17 +0200

    Format and print records

--- utils.go
+++ utils.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
+)
+
+var (
+	leftPadding int = 22
 )
 
 func readConfig() *Config {
@@ -18,4 +24,31 @@ func readConfig() *Config {
 		log.Fatal("readConfig-Unmarshal", err)
 	}
 	return config
+}
+
+func filterRecords(m MultipleRecords) {
+	stdoutHeader()
+	for _, item := range m {
+		r := item["record"]
+		// Skip system records are managed by DNSimple
+		if !r.SystemRecord {
+			stdoutRecord(r)
+		}
+	}
+}
+
+func stdoutHeader() {
+	headerFmt := fmt.Sprintf("%s%21s%22s%26s", "Type", "Name", "TTL", "Content")
+	fmt.Println(headerFmt)
+}
+
+// Print out a record according the leftpadding
+func stdoutRecord(r Record) {
+	recordType := "%" + fmt.Sprintf("%d", (leftPadding-len(r.RecordType))+len(r.Name+config.Domain)) + "s"
+	recordName := "%" + fmt.Sprintf("%d", (leftPadding-len(r.Name+config.Domain))+len(strconv.Itoa(r.TTL))) + "d"
+	recordTTL := "%" + fmt.Sprintf("%d", (leftPadding-len(strconv.Itoa(r.TTL)))+len(r.Content)) + "s"
+	data := fmt.Sprint("%s", recordType, recordName, recordTTL)
+	formatEnd := fmt.Sprintf("%s", data)
+	fmt.Printf(fmt.Sprintf(formatEnd, r.RecordType, r.Name+"."+config.Domain, r.TTL, r.Content))
+	fmt.Println("")
 }
