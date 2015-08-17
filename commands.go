ff4ca84fc27af47da7e1fdad0301963953b9bc04
Sergio Galvan <sergalma@gmail.com>
2015-08-17 19:52:57 +0200

    Command order

--- commands.go
+++ commands.go
@@ -12,8 +12,75 @@ var commands = []cli.Command{
 		Usage: "manage records",
 		Subcommands: []cli.Command{
 			{
+				Name:  "add",
+				Usage: "create record",
+				Action: func(c *cli.Context) {
+					domain := c.GlobalString("domain")
+					recordType := c.String("type")
+					content := c.String("content")
+					name := c.String("name")
+					if content == "" || recordType == "" {
+						cli.ShowSubcommandHelp(c)
+						return
+					}
+					createRecordDomain(domain, name, content, recordType)
+				},
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "type, t",
+						Usage: "record type (i.e: A, NS, MX)",
+					},
+					cli.StringFlag{
+						Name:  "content, c",
+						Usage: "record content",
+					},
+					cli.StringFlag{
+						Name:  "name, n",
+						Usage: "record name",
+					},
+				},
+			},
+			{
+				Name:  "delete",
+				Usage: "delete record",
+				Action: func(c *cli.Context) {
+					domain := c.GlobalString("domain")
+					id := c.String("id")
+					if id == "" {
+						cli.ShowSubcommandHelp(c)
+						return
+					}
+					deleteRecordDomain(domain, id)
+				},
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "id, i",
+						Usage: "id record",
+					},
+				},
+			},
+			{
+				Name:  "get",
+				Usage: "get record",
+				Action: func(c *cli.Context) {
+					domain := c.GlobalString("domain")
+					id := c.String("id")
+					if id == "" {
+						cli.ShowSubcommandHelp(c)
+						return
+					}
+					getRecordDomain(domain, id)
+				},
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "id, i",
+						Usage: "id record",
+					},
+				},
+			},
+			{
 				Name:  "list",
-				Usage: "list records domain",
+				Usage: "list records",
 				Action: func(c *cli.Context) {
 					domain := c.GlobalString("domain")
 					listRecordsDomain(domain)
@@ -40,73 +107,6 @@ var commands = []cli.Command{
 					cli.StringFlag{
 						Name:  "content, c",
 						Usage: "record value",
-					},
-				},
-			},
-			{
-				Name:  "get",
-				Usage: "get record for a domain",
-				Action: func(c *cli.Context) {
-					domain := c.GlobalString("domain")
-					id := c.String("id")
-					if id == "" {
-						cli.ShowSubcommandHelp(c)
-						return
-					}
-					getRecordDomain(domain, id)
-				},
-				Flags: []cli.Flag{
-					cli.StringFlag{
-						Name:  "id, i",
-						Usage: "id record",
-					},
-				},
-			},
-			{
-				Name:  "delete",
-				Usage: "delete record",
-				Action: func(c *cli.Context) {
-					domain := c.GlobalString("domain")
-					id := c.String("id")
-					if id == "" {
-						cli.ShowSubcommandHelp(c)
-						return
-					}
-					deleteRecordDomain(domain, id)
-				},
-				Flags: []cli.Flag{
-					cli.StringFlag{
-						Name:  "id, i",
-						Usage: "id record",
-					},
-				},
-			},
-			{
-				Name:  "add",
-				Usage: "create record for a domain",
-				Action: func(c *cli.Context) {
-					domain := c.GlobalString("domain")
-					recordType := c.String("type")
-					content := c.String("content")
-					name := c.String("name")
-					if content == "" || recordType == "" {
-						cli.ShowSubcommandHelp(c)
-						return
-					}
-					createRecordDomain(domain, name, content, recordType)
-				},
-				Flags: []cli.Flag{
-					cli.StringFlag{
-						Name:  "type, t",
-						Usage: "record type (i.e: A, NS, MX)",
-					},
-					cli.StringFlag{
-						Name:  "content, c",
-						Usage: "record content",
-					},
-					cli.StringFlag{
-						Name:  "name, n",
-						Usage: "record name",
 					},
 				},
 			},
